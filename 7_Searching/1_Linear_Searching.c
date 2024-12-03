3342a07c2001938e6c504c3146228a498dd66663
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2024-12-03 21:49:45 +0530

    change linear search

--- 7_Searching/1_Linear_Searching.c
+++ 7_Searching/1_Linear_Searching.c
@@ -4,19 +4,20 @@
 int arr[ARRAY_SIZE] = {1,2,3,4,5,6,7,8,9,10};
 
 int LinearSearch(int value){
-    for(int i=0; i<ARRAY_SIZE; i++){
-        if(arr[i]==value)
-            return i+1;
+    int i, isFound=0;
+    for(i=0; i<ARRAY_SIZE; i++){
+        if(arr[i]==value){
+            isFound = 1;
+            printf("Element is present at %d position in array.", i+1);
+            break;
+        }
     }
-    return -1;
+    if(!isFound){
+        printf("Element is not present in the array.");
+    }
 }
 
 int main(){
-    int add = LinearSearch(10);
-    if(add==-1){
-        printf("Element is not present in the array.");
-    }else{
-        printf("Element is present at %d position in array.", add);
-    }
+    LinearSearch(20);
     return 0;
 }

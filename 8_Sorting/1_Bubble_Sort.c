8407ae3488061a95f3783a94e73458316a2c9c3a
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2024-12-04 13:40:24 +0530

    add bubble sort

---
+++ 8_Sorting/1_Bubble_Sort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+#define ARRAY_SIZE 10
+int arr[ARRAY_SIZE] = {6,7,2,10,8,1,4,9,5,3};
+
+void Swap(int *a, int *b){
+    *a = *a+*b;
+    *b = *a-*b;
+    *a = *a-*b;
+}
+
+void BubbleSort(){
+    for(int i = 0; i<ARRAY_SIZE-1; i++){
+        for(int j=0;j<ARRAY_SIZE-1;j++){
+            if(arr[j]>arr[j+1])
+                Swap(&arr[j], &arr[j+1]);
+        }
+    }
+}
+
+void Display(){
+    for(int i=0;i<ARRAY_SIZE;i++){
+        printf("%d  ", arr[i]);
+    }
+}
+
+int main(){
+    printf("Before Sort :- ");
+    Display();
+    BubbleSort();
+    printf("\nAfter Sort :- ");
+    Display();
+    return 0;
+}

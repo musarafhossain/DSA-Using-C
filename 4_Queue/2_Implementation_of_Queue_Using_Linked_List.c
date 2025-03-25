3ccd563879a69999166980af50046efe2c256960
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2025-03-25 18:01:28 +0530

    add queue implementation using linked list

---
+++ 4_Queue/2_Implementation_of_Queue_Using_Linked_List.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+struct node *front = NULL;
+struct node *rear = NULL;
+
+int isEmpty(){
+    return front==NULL;
+}
+
+void enqueue(int X){
+    struct node *newnode;
+    newnode = (struct node *)malloc(sizeof(struct node));
+    if (newnode == NULL) {
+        printf("\nMemory allocation failed!");
+        return;
+    }
+    newnode->data = X;
+    newnode->next = NULL;
+    if(isEmpty()){
+        front = newnode;
+        rear = newnode;
+    } else{
+        rear->next = newnode;
+        rear = newnode;
+    }
+}
+
+void dequeue(){
+    struct node *temp;
+    if(isEmpty()){
+        rear == NULL;
+        printf("\nUnderflow!");
+    } else{
+        printf("\nDeleted Item : %d", front->data);
+        temp=front;
+        front = front->next;
+        free(temp);
+    }
+}
+
+void display(){
+    struct node *temp;
+    if(isEmpty()){
+        printf("\nQueue is Empty!");
+    } else{
+        temp=front;
+        printf("\nQueue Elements : ");
+        while(temp!=NULL){
+            printf("%d ", temp->data);
+            temp = temp->next;
+        }
+    }
+}
+
+int main(){
+    enqueue(0);
+    enqueue(1);
+    enqueue(2);
+    enqueue(3);
+    enqueue(4);
+    dequeue();
+    dequeue();
+    dequeue();
+    dequeue();
+    dequeue();
+    dequeue();
+    dequeue();
+    display();
+    return 0;
+}
